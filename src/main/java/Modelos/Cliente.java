3ba4a76c896491505eaf7ad402cd756bd7ad88a9
Owen <bareno.owen.9508@eam.edu.co>
2025-02-26 22:49:39 -0500

    AGREGADO modelo cliente

---
+++ src/main/java/Modelos/Cliente.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author PC
+ */
+public class Cliente {
+     private String nombre;
+    private String apellido;
+    private String cedula;
+    private String telefono;
+    private String placaVehiculo;
+
+    // Constructor
+    public Cliente(String nombre, String apellido, String cedula, String telefono, String placaVehiculo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.telefono = telefono;
+        this.placaVehiculo = placaVehiculo;
+        this.factura = null; // La factura se generará después
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPlacaVehiculo() {
+        return placaVehiculo;
+    }
+
+    public void setPlacaVehiculo(String placaVehiculo) {
+        this.placaVehiculo = placaVehiculo;
+    }
+
+   
+
+}
