38e53459f58856d06dc6f901241445527a308afd
dany <dani06agu@gmail.com>
2025-03-03 22:27:18 -0500

    Modificado ControladorFactura

--- src/main/java/Controlador/ControladorFactura.java
+++ src/main/java/Controlador/ControladorFactura.java
@@ -4,25 +4,23 @@
  */
 package Controlador;
 
-/**
- *
- * @author PC
- */
 import Servicios.ServicioFactura;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class ControladorFactura {
     private ServicioFactura servicioFactura;
-    private Scanner scanner;
 
-    public ControladorFactura(ServicioFactura servicioFactura, Scanner scanner) {
+    public ControladorFactura(ServicioFactura servicioFactura) {
         this.servicioFactura = servicioFactura;
-        this.scanner = scanner;
     }
 
     public void generarFactura() {
-        System.out.print("Ingrese la placa del vehículo: ");
-        String placaFactura = scanner.nextLine(); // Capturar la placa
-        servicioFactura.generarFactura(placaFactura); // Llamada correcta al método en ServicioFactura
+        String placaFactura = JOptionPane.showInputDialog("Ingrese la placa del vehículo:");
+
+        if (placaFactura != null && !placaFactura.trim().isEmpty()) {
+            servicioFactura.generarFactura(placaFactura);
+        } else {
+            JOptionPane.showMessageDialog(null, "Placa inválida o operación cancelada.", "Advertencia", JOptionPane.WARNING_MESSAGE);
+        }
     }
 }
